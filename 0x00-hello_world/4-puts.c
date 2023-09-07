fb0382a0bca87df95f656d4e1a8ea193edb4dccf
prince <pr_mustafa@outlook.com>
2023-09-07 16:12:09 -0700

    commiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
