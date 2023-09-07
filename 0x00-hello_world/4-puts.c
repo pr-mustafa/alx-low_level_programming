d99911d41985b4f404a1919853f89ef13dda9fef
prince <pr_mustafa@outlook.com>
2023-09-07 16:03:59 -0700

    commiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	/*string initialisation*/
+    	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+    	puts(Mystr); /*writing the string to stdout*/
 	return (0);
 }
