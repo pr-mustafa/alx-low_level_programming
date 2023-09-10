6f83132a36424857876fb497bac7501846d81105
prince <pr_mustafa@outlook.com>
2023-09-10 12:44:33 -0700

    commiting

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+        {
+                putchar(c);
+        }
+	putchar('\n');
+	return (0);
+}
