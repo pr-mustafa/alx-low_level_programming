8c6626227edf8f317369196cc176eb5fc5cc2493
prince <pr_mustafa@outlook.com>
2023-09-10 12:49:34 -0700

    commiting

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' || c != 'e')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
