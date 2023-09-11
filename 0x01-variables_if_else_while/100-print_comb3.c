93b63b905a910db62712d5d3933a88c51eafae32
prince <pr_mustafa@outlook.com>
2023-09-11 06:03:20 -0700

    commiting

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int c;
+	int x;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (x > c)
+			{
+				putchar('0'+c);
+				putchar('0'+x);
+				if (c == 8 && x == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
