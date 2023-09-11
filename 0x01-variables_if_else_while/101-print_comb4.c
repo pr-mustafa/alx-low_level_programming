cd63c0296ad07f097aeb9c0ac5fa7c3540e72412
prince <pr_mustafa@outlook.com>
2023-09-11 06:16:57 -0700

    commiting

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,14 +15,14 @@ int main(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			for (k = 0; k<= 9; k++)
+			for (k = 0; k <= 9; k++)
 			{
 				if (j > i && k > j)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
 					putchar('0' + k);
-					if (i == 7 && j == 8 && k==9)
+					if (i == 7 && j == 8 && k == 9)
 					{
 						break;
 					}
