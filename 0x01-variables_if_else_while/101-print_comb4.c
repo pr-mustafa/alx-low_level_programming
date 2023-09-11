c8089b0cef6f2618241da65a9d56c97e32f0fffc
prince <pr_mustafa@outlook.com>
2023-09-11 06:15:15 -0700

    commiting

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k<= 9; k++)
+			{
+				if (j > i && k > j)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i == 7 && j == 8 && k==9)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
