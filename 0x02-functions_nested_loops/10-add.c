fff14d8a9b79e7254665fb99f73482ca622e9644
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 14:24:27 -0700

    Task try push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * add - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+	/* your code goes there */
+	int sum;
+	
+	sum = x + y;
+	return (sum);
+}
