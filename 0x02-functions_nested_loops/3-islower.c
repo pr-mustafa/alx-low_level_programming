54beb7ec27440e237fb01b0fe6c782636147ae87
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 08:57:27 -0700

    Task try push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,13 @@
  * Description: This function prints 1 if it is lower else print 0
  *
  * Return: void
+ *
+ * int c is the character to be checked
  */
 int _islower(int c)
 {
-	/* c is the char that will be checked */
+	/*int c is the char that will be checked */
+
 	if (islower(c))
 	{
 		return (1);
