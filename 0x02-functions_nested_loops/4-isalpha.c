6506db2fb183041d8d3a045c16db121e7b480d9b
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 09:16:49 -0700

    Task try push

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for lowercase
+ *
+ * @c: is the character to be checked
+ */
+
+int _isalpha(int c)
+{
+
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
