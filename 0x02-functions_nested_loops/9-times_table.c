f649570f45ce9b6754f5a98f628cd9cdb878faaf
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 14:14:38 -0700

    Task try push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,18 @@
   */
 void times_table(void)
 {
-        int a, b, c;
-
-        for (a = 0; a <= 9; a++)
-        {
-                for (b = 0; b <=9; b++)
-                {
-                        c = a * b;
+	int a, b, c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <=9; b++)
+		{
+			c = a * b;
 			if (c >= 10)
 			{
 				_putchar(' ');
-				_putchar((c/10) + '0');
-				_putchar((c%10) + '0');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 			else if (b == 0)
 			{
