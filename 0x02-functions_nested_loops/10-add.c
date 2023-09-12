2a9283edfe14014875fd3b23bb52b755e359898f
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 14:27:24 -0700

    Task try push

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,14 @@
 /**
  * add - Entry point
  *
+ * @x: first parm
+ * @y: second parm
  * Return: Always 0 (Success)
  */
 int add(int x, int y)
 {
-	/* your code goes there */
 	int sum;
-	
+
 	sum = x + y;
 	return (sum);
 }
