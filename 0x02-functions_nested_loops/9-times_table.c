e0bf0d23c0edd608db98166a440d9425c255282d
pr-mustafa <pr_mustafa@outlook.com>
2023-09-12 14:03:43 -0700

    Task try push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * times_table - Prints every minute of the day of Jack Bauer
+  *
+  * Return: ...
+  */
+void times_table(void)
+{
+        int a, b, c;
+
+        for (a = 0; a <= 9; a++)
+        {
+                for (b = 0; b <=9; b++)
+                {
+                        c = a * b;
+			if (c >= 10)
+			{
+				_putchar((c/10) + '0');
+				_putchar((c%10) + '0');
+			}
+			else
+			{
+				_putchar(c + '0');
+			}
+			if ( b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+                }
+		_putchar('\n');
+        }
+        
+}
