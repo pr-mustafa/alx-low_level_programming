49f59f8dfceace0b86beecb78eb93b03a543796a
pr-mustafa <pr_mustafa@outlook.com>
2023-09-13 12:17:26 -0700

    Task try push

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * print_times_table - Prints every minute of the day of Jack Bauer
+  *
+  * Return: ...
+  */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c >= 10 && c < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c >= 100)
+				{
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				if (b != n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
