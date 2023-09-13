4082cebd2f8da1ec4b52db75afedc4e95b16cd13
pr-mustafa <pr_mustafa@outlook.com>
2023-09-13 13:00:00 -0700

    Task try push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,49 @@
 #include "main.h"
-
 /**
   * print_times_table - Prints every minute of the day of Jack Bauer
-  *
+  *@n: parm
   * Return: ...
   */
 void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 0 || n > 15)
+		return;
+	for (a = 0; a <= n; a++)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			c = a * b;
+			if (b == 0)
 			{
-				c = a * b;
-				if (c >= 10 && c < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
-				else if (c >= 100)
-				{
-					_putchar(' ');
-					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');
-				}
-				else if (b == 0)
-				{
-					_putchar(c + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				if (b != n)
-				{
-					_putchar(',');
-				}
+				_putchar('0');
+				continue;
 			}
-			_putchar('\n');
+			_putchar(' ');
+			if (c >= 10 && c < 100)
+			{
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (c >= 100)
+			{
+				_putchar((c / 100) + '0');
+				_putchar(((c / 10) % 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (c >= 0 && c <= 9 && b != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			if (b != n)
+			{
+				_putchar(',');
+			}
 		}
+			_putchar('\n');
 	}
 }
