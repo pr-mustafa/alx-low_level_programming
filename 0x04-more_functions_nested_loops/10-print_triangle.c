a763574ada7999864c9ff4d6167a32eaa111ed65
pr-mustafa <pr_mustafa@outlook.com>
2023-09-14 09:16:52 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_triangle - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for upercase
+ *
+ * @n: is the lenght of line
+ */
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if ((i + j) < (n-1))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
