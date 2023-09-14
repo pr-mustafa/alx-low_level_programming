f6cf0d458755c6252356ba1f6f6d34e85a88e18b
pr-mustafa <pr_mustafa@outlook.com>
2023-09-14 06:35:16 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for upercase
+ *
+ * @a: first parm
+ * @b: second parm
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
