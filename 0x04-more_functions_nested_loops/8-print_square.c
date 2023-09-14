65706cf278761e4d2100fc03f23d47ba01e2a9ce
pr-mustafa <pr_mustafa@outlook.com>
2023-09-14 08:26:50 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_square - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for upercase
+ *
+ * @n: is the lenght of line
+ */
+
+void print_square(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
