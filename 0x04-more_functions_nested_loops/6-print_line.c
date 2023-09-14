f6357288e14bc5c03150b73d90dc70e7694c1b73
pr-mustafa <pr_mustafa@outlook.com>
2023-09-14 07:29:33 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_line - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for upercase
+ *
+ * @n: is the lenght of line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
