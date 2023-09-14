57abf81d481f3980620355114f95e7eb7ef920f1
pr-mustafa <pr_mustafa@outlook.com>
2023-09-14 12:28:37 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i = 1231952;
+	int div;
+
+	while (i >= 2)
+	{
+		if (i % 2 == 0)
+		{
+
+		}
+		else if (i % 3 == 0)
+		{
+		}
+
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,51 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_number - entry point
+ *
+ * Description: This function prints 1 if it is lower else print 0
+ *
+ * Return: 1 for upercase
+ *
+ * @n: is the character to be checked
+ */
+
+void print_number(int n)
+{
+	int length, div, max, flag;
+
+	flag = 0;
+	if (n < 0)
+	{
+		flag = 1;
+		n = -n;
+	}
+	length = 1;
+	max = 1;
+	div = n / 10;
+	while (div != 0)
+	{
+		length++;
+		max *= 10;
+		div = div / 10;
+	}
+	if (flag == 1)
+	{
+		_putchar('-');
+	}
+	if (length == 1)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		_putchar((n / max) + '0');
+		while (length != 1)
+		{
+			n = n % max;
+			max /= 10;
+			_putchar((n / max) + '0');
+			length--;
+		}
+	}
+}
