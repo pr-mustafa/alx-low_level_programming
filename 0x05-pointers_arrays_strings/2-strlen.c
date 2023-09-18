c4b0b28370add74b389b6d67c154af3921e5c5e0
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 08:49:33 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *swap_int - change the value
+ *@a: pointer of a
+ *@b: pointer of b
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	/* your code goes there */
+	return strlen(s);
+}
