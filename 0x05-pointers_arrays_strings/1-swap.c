df0f1bb48b3cea24628c9877d4667136cea65a3c
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 08:30:48 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - change the value
+ *@a: pointer of a
+ *@b: pointer of b
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	/* your code goes there */
+	*a = 42;
+	*b = 98;
+}
