aa888e9f14a75f939fbc08e209f800234f375608
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 14:36:51 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *_strcpy - change the value
+ *@dest: pointer of a
+ *@src: size of elements to be print
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
