f1e046e4c8746e1a40fe8d3c8b0d0d264eac3fe9
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 08:21:13 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - change the value
+ *@n: pointer of n
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	/* your code goes there */
+	*n = 98;
+}
