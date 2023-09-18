b9f3d2f9c7bfc6104278dd87d6f0723f4a6aedbb
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 09:47:34 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - print the value
+ *@str: pointer of str
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	/* your code goes there */
+	puts(str);
+}
