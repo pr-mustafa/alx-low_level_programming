5f06fa0b013719eccd615eda486e12857acede99
pr-mustafa <pr_mustafa@outlook.com>
2023-09-18 11:41:27 -0700

    commiting

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<string.h>
+/**
+ *print_rev - change the value
+ *@s: pointer of s
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int i, len;
+	len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_rev - change the value
+ *@s: pointer of s
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int i;
+	char *a = *s;
+
+
+}
