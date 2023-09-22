d9127b3e7d47dde888009c42dcfc334693279c1e
pr-mustafa <pr_mustafa@outlook.com>
2023-09-22 10:11:21 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcat - change the value
+ *@a: pointer of a
+ *@b: pointer of b
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *a, char *b)
+{
+	/* your code goes there */
+	char *c = strcat(a, b);
+	return (c);
+}
