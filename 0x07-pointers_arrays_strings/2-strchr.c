4a644c9fdea06f25d644e9e65c4dad641cd783d0
pr-mustafa <pr_mustafa@outlook.com>
2023-09-26 01:19:25 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strchr - change the value
+ *@s: pointer of string
+ *@c: character to be search
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	/* your code goes there */
+	int i, sum;
+	char *p = NULL;
+	
+	sum = 0;
+	while (s[sum] != '\0')
+	{
+		sum += 1;
+	}
+
+	for (i = 0; i < sum; i++)
+	{
+		if (s[i] == c)
+			{
+				p = &s[i];
+				return (p);
+			}
+	}
+	return (p);
+}
