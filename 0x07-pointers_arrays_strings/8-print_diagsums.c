78fb26cb962520e21b8f42080cfa1eb25123581d
pr-mustafa <pr_mustafa@outlook.com>
2023-09-26 12:17:21 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints the sum 
+ * print_diagsums - prints the sum
  *
  * @a: the name of the array
  * @size: the size of the array
