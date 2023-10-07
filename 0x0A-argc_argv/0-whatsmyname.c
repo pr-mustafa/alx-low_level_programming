d755d0b2d91895cf664d3f1fa5a8a669532697d8
pr-mustafa <pr_mustafa@outlook.com>
2023-10-07 06:32:34 -0700

    commiting

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always argc.
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
