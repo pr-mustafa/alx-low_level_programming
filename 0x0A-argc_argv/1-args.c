e7c400060fdd6647366b6876cfebc27091cbd694
pr-mustafa <pr_mustafa@outlook.com>
2023-10-07 06:39:50 -0700

    commiting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always argc.
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
