0a7e45a6d2fe49423338f3d81bc4a5c35738ec85
pr-mustafa <pr_mustafa@outlook.com>
2023-10-10 13:48:22 -0700

    commiting

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef abs_h
-#define abs_h
-#define ABS(x) (x > 0 ? x : x * -1)
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+
 #endif
