2050316300c08032e504f434aec9b1f98558138f
pr-mustafa <pr_mustafa@outlook.com>
2023-10-10 14:22:28 -0700

    commiting

---
+++ dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - struct with dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: store different data types
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_s;
+
+#endif
