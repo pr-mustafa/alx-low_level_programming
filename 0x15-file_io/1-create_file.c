b8cc90ff442369bd476dc68ba211523874f622f3
pr-mustafa <pr_mustafa@outlook.com>
2023-11-02 04:24:09 -0700

    commiting

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ * @filename: The file's name.
+ * @text_content: The file's content.
+ * Return: 1 on sucess or -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fdesc, error, size = 0;
+
+	if (filename)
+	{
+		fdesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fdesc > 0)
+		{
+			while (text_content && text_content[size] != '\0')
+				size++;
+			error = write(fdesc, text_content, size);
+			close(fdesc);
+			return ((error >= 0) ? 1 : -1);
+		}
+	}
+	return (-1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include "main.h"
+
+int usage_msg(void);
+int cant_read(char *);
+int cant_write(char *);
+int cant_close(int);
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: pointer to array of arguments.
+ * Return: 1 on sucess or -1 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	char buffer[1024];
+	int fd_origin, fd_destiny, size = 1024, error = -1, buff_size = 1024;
+
+	if (argc != 3)
+		exit(usage_msg());
+	fd_origin  = open(argv[1], O_RDONLY);
+	if (fd_origin > 0)
+	{
+		fd_destiny = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (fd_destiny > 0)
+		{
+			while (size == 1024)
+			{
+				size = read(fd_origin, buffer, buff_size);
+				if (size < 0)
+				{
+					close(fd_origin);
+					close(fd_destiny);
+					exit(cant_read(argv[1]));
+				}
+				error = write(fd_destiny, buffer, size);
+				if (error < 0)
+				{
+					close(fd_origin);
+					close(fd_destiny);
+					exit(cant_write(argv[2]));
+				}
+			}
+		}
+		else
+			exit(cant_write(argv[2]));
+		if (close(fd_origin) < 0)
+			exit(cant_close(fd_origin));
+		if (close(fd_destiny) < 0)
+			exit(cant_close(fd_destiny));
+	}
+	else
+	{
+		close(fd_origin);
+		exit(cant_read(argv[1]));
+	}
+	return (0);
+}
+
+/**
+ * usage_msg - print error message to describe usage.
+ * Return: 97 for exit param.
+ */
+int usage_msg(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	return (97);
+}
+
+/**
+ * cant_read - print error message for reading error.
+ * @fdin: File descriptor for input.
+ * Return: 98 for exit param.
+ */
+int cant_read(char *fdin)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fdin);
+	return (98);
+}
+
+/**
+ * cant_write - print error message for writing error.
+ * @fdout: File descriptor for output.
+ * Return: 99 for exit param.
+ */
+int cant_write(char *fdout)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fdout);
+	return (99);
+}
+
+/**
+ * cant_close - print error message for closing error.
+ * @fd: File descriptor for closing file.
+ * Return: 100 for exit param.
+ */
+int cant_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+	return (100);
+}
